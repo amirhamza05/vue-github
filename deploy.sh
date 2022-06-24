73d86082145f719e371c89769323893ee014f35b
amirhamza05 <amir.hamza@streamstech.com>
2022-06-25 04:26:05 +0600

    Update Feature

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,11 @@
 echo "deploying===========================";
+
+echo '============================ Build Docker Compose File ===========================';
+docker-compose build
+
+echo '===================== Stop and Remove Docker Container ===========================';
 docker stop github-vue
 docker rm github-vue
 
+echo '============================ Run Docker Container ================================';
 docker-compose up -d
